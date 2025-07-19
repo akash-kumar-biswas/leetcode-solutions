581a5208addb0bf8e5917d6dc3fc89c5fa6577a3
Akash Biswas <akash41bt@gmail.com>
2025-07-20 02:19:09 +0600

    solve LeetCode 1318: in O(1) time

--- Medium/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ Medium/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,30 +1,8 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int  i = 0;
-        int cnt = 0;
-        while(i <= 32){
-            if((c & 1) == 1){
-                if(!(a & 1) && !(b & 1)){
-                    cnt++;
-                }
-            }
-            else{
-                if((a & 1) && (b & 1)){
-                    cnt+=2;
-                }
-                else if((a&1)){
-                    cnt++;
-                }
-                else if(b&1){
-                   cnt++; 
-                }
-            }
-            i++;
-            c >>= 1;
-            a >>= 1;
-            b >>= 1;
-        }
-        return cnt;
+        int x_or = (a|b) ^ c;
+        return __builtin_popcount(x_or) + 
+        __builtin_popcount(x_or & (a&b));
     }
 };
