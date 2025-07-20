fb9ef327dd81297124693844bef1ac37cf721b3f
Akash Biswas <akash41bt@gmail.com>
2025-07-20 06:39:35 +0600

    solve LeetCode 201: in another approach of bit manipulation

--- Medium/201-bitwise-and-of-numbers-range.cpp
+++ Medium/201-bitwise-and-of-numbers-range.cpp
@@ -1,34 +1,13 @@
 class Solution {
 public:
-    void positionUnsetBit(vector<int> &map, int num){
+    int rangeBitwiseAnd(int left, int right) {
         int k = 0;
 
-        while(k < 32){
-            if((num & 1) == 0)
-                map[k] = 0;
-            num >>= 1;
+        while(left != right){
+            left >>= 1;
+            right >>= 1;
             k++;
         }
-    }
-
-    int rangeBitwiseAnd(int left, int right) {
-        vector<int> map(32, 1);
-        positionUnsetBit(map, right);
-        int i = 0;
-        long long _left = left;
-        while(_left < right){
-            if(find(map.begin(), map.end(), 1) != map.end())
-                positionUnsetBit(map, _left);
-            else
-                break;
-            _left += 1<<i;
-            i++;
-        }
-        int ans = 0;
-        for(int k = 0; k < 32; k++){
-            if(map[k] == 1)
-                ans |= (1<<k);
-        }
-        return ans;
+        return left << k;
     }
 };
