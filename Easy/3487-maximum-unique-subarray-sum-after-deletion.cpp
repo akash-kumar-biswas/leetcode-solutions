4cb2ee7ffe9b8bb2325c8a3c2727153d805c9fe1
Akash Biswas <akash41bt@gmail.com>
2025-07-25 14:02:18 +0600

    solve LeetCode 3487: using hashTable

---
+++ Easy/3487-maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> ust;
+        int neg_max = INT_MIN;
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] >= 0)
+                ust.insert(nums[i]);
+            else
+                neg_max = max (neg_max, nums[i]);
+        }
+        if(!ust.size())
+            return neg_max;
+        int ans = 0;
+
+        for(auto it: ust){
+            ans += it;
+        }
+        return ans;
+    }
+};
