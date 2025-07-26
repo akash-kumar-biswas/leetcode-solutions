ebdcffdced627ab5fa91455eff8acdd1e835cfe7
Akash Biswas <akash41bt@gmail.com>
2025-07-26 12:56:27 +0600

    update

--- Easy/3487-maximum-unique-subarray-sum-after-deletion.cpp
+++ Easy/3487-maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,21 +1,25 @@
-class Solution {
+class Solution
+{
 public:
-    int maxSum(vector<int>& nums) {
+    int maxSum(vector<int> &nums)
+    {
         int n = nums.size();
         unordered_set<int> ust;
         int neg_max = INT_MIN;
 
-        for(int i = 0; i < n; i++){
-            if(nums[i] >= 0)
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] >= 0)
                 ust.insert(nums[i]);
             else
-                neg_max = max (neg_max, nums[i]);
+                neg_max = max(neg_max, nums[i]);
         }
-        if(!ust.size())
+        if (!ust.size())
             return neg_max;
         int ans = 0;
 
-        for(auto it: ust){
+        for (auto it : ust)
+        {
             ans += it;
         }
         return ans;
