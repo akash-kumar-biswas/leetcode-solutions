a0ef12f16f919def758b5993414c11d0738598bd
Akash Biswas <akash41bt@gmail.com>
2025-09-02 17:18:13 +0600

    solve LeetCode 49: using hashtable and without sorting (O(n*k) time)

--- Medium/49-group-anagrams.cpp
+++ Medium/49-group-anagrams.cpp
@@ -1,15 +1,36 @@
-class Solution {
+class Solution
+{
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    string withoutSorting(vector<int> &arr, string str)
+    {
+        for (auto cha : str)
+        {
+            arr[cha - 'a']++;
+        }
+
+        string temp;
+        for (int i = 0; i < 26; i++)
+        {
+            char cha = 'a' + i;
+            while (arr[i]--)
+                temp.push_back(cha);
+        }
+        return temp;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
         unordered_map<string, vector<string>> ump;
-        for(auto str: strs){
-            string temp = str;
-            sort(temp.begin(), temp.end());
+        for (auto str : strs)
+        {
+            vector<int> arr(26, 0);
+            string temp = withoutSorting(arr, str);
             ump[temp].push_back(str);
         }
-        
+
         vector<vector<string>> ans;
-        for(auto it: ump){
+        for (auto it : ump)
+        {
             ans.push_back(it.second);
         }
         return ans;
